ec4f99faca5e3574be879b6e7497dd4ef689dd5b
rodrigo <rdsouza.c@gmail.com>
2021-08-26 18:16:12 -0400

    Configurando o Resource Server para JWT assinado com chave simétrica

--- souzafood-api/src/main/java/com/souza/souzafood/core/security/ResourceServerSecurityConfig.java
+++ souzafood-api/src/main/java/com/souza/souzafood/core/security/ResourceServerSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.souza.souzafood.core.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,8 +20,15 @@ public class ResourceServerSecurityConfig extends WebSecurityConfigurerAdapter {
 	       .authorizeRequests()
 	          .anyRequest().authenticated()
 	       .and()
-	       .cors().and() //Aula: https://app.algaworks.com/aulas/2246/testando-o-fluxo-authorization-code-com-um-client-javascript
-	       .oauth2ResourceServer().opaqueToken();
+	       .cors().and() // Sobre o metodo cors(). Aula: https://app.algaworks.com/aulas/2246/testando-o-fluxo-authorization-code-com-um-client-javascript
+	       .oauth2ResourceServer().jwt();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		var secretKey = new SecretKeySpec("KJNFUHWKFWKF98723721NQDLQNDQDJ81UNLDNadasldnasldnladas123".getBytes(), "HmacSHA256");
+		
+		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 	
 }
