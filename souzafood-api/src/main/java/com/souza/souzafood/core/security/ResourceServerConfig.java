7103d688ff6c7a993af60f0f435d631a862cc296
rodrigo <rdsouza.c@gmail.com>
2021-09-15 23:20:14 -0400

    Ajustando a documentação da API para suporte a OAuth2

--- souzafood-api/src/main/java/com/souza/souzafood/core/security/ResourceServerConfig.java
+++ souzafood-api/src/main/java/com/souza/souzafood/core/security/ResourceServerConfig.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,6 +24,11 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		    .formLogin()
+		    .and()
+		    .authorizeRequests()
+		        .antMatchers("/oauth/**").authenticated()
+		        .and()
 		    .csrf().disable() // Sobre o metodo .csrf() Aula: https://app.algaworks.com/aulas/2230/configurando-spring-security-com-http-basic
 	                                                      // Aula: https://app.algaworks.com/aulas/1490/implementando-autenticacao-basic
 		    .cors().and() // Sobre o metodo .cors() Aula: https://app.algaworks.com/aulas/2246/testando-o-fluxo-authorization-code-com-um-client-javascript
@@ -52,6 +59,14 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 		});
 		
 		return jwtAuthenticationConverter;
+	}
+	
+	
+//	https://app.algaworks.com/aulas/2295/juntando-o-resource-server-com-o-authorization-server-no-mesmo-projeto
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 	
 //	Aula: https://app.algaworks.com/aulas/2260/configurando-o-resource-server-para-jwt-assinado-com-chave-simetrica
--- souzafood-api/src/main/java/com/souza/souzafood/core/web/WebConfig.java
+++ souzafood-api/src/main/java/com/souza/souzafood/core/web/WebConfig.java
@@ -2,39 +2,40 @@ package com.souza.souzafood.core.web;
 
 import javax.servlet.Filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-	@Autowired
-	private ApiRetirementHandler apiRetirementHandler;
-	
-//	https://app.algaworks.com/aulas/2099/habilitando-cors-globalmente-no-projeto-da-api
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-
-		registry.addMapping("/**")
-		        .allowedMethods("*");
-//		        .allowedOrigins("*")
-//		        .maxAge(30);   
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-	registry.addInterceptor(apiRetirementHandler);
-	}
 	
 //	https://app.algaworks.com/aulas/2110/implementando-requisicoes-condicionais-com-shallow-etags
 	@Bean
 	public Filter shallowEtagHeaderFilter() {
 		return new ShallowEtagHeaderFilter();
 	}
+
+//	https://app.algaworks.com/aulas/2221/depreciando-uma-versao-da-api
+//	@Autowired
+//	private ApiRetirementHandler apiRetirementHandler;
+
+//	Tiramos a configuracao abaixo na aula: https://app.algaworks.com/aulas/2295/juntando-o-resource-server-com-o-authorization-server-no-mesmo-projeto
+	
+//	https://app.algaworks.com/aulas/2099/habilitando-cors-globalmente-no-projeto-da-api
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//
+//		registry.addMapping("/**")
+//  		       .allowedMethods("*");
+//	    	       .allowedOrigins("*")
+//		           .maxAge(30);   
+//	}
+
+//	https://app.algaworks.com/aulas/2221/depreciando-uma-versao-da-api
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//	registry.addInterceptor(apiRetirementHandler);
+//	}
 
 }
