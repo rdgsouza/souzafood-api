392eb407102d4199b3cee826efb52f9e63ebbafd
rodrigo <rdsouza.c@gmail.com>
2021-10-04 21:20:39 -0400

    Escalando um serviço com Docker Compose

--- souzafood-api/src/main/resources/db/testdata/afterMigrate.sql
+++ souzafood-api/src/main/resources/db/testdata/afterMigrate.sql
@@ -1,5 +1,10 @@
-
 set foreign_key_checks = 0;
+
+lock tables cidade write, cozinha write, estado write, forma_pagamento write,
+	grupo write, grupo_permissao write, permissao write,
+	produto write, restaurante write, restaurante_forma_pagamento write,
+	restaurante_usuario_responsavel write, usuario write, usuario_grupo write,
+	pedido write, item_pedido write, foto_produto write, oauth_client_details write; 
 
 truncate souzafood.cidade;
 truncate souzafood.cozinha;
@@ -209,3 +214,5 @@ values (
   'READ,WRITE', 'client_credentials', null, 'CONSULTAR_PEDIDOS,GERAR_RELATORIOS',
   null, null, null
 );
+
+unlock tables;
