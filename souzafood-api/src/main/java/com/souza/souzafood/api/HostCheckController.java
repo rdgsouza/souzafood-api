3d687518f709ae48fe0ddf6d802ac01f9c629682
rodrigo <rdsouza.c@gmail.com>
2021-10-11 18:27:17 -0400

    souzafood-api

---
+++ souzafood-api/src/main/java/com/souza/souzafood/api/HostCheckController.java
@@ -0,0 +1,20 @@
+package com.souza.souzafood.api;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HostCheckController {
+
+	@GetMapping("/hostcheck")
+	public String checksHost() throws UnknownHostException {
+		
+		return InetAddress.getLocalHost().getHostAddress()
+				+ " - " + InetAddress.getLocalHost().getHostName();
+		
+	}
+	
+}
